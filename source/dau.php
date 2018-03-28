cf50a9fb1ef5f150b89ac6d0927357faf360e38b
yaxixi <cx0cx2001@aliyun.com>
2018-03-28 23:41:29 +0800

    代码提交

--- source/dau.php
+++ source/dau.php
@@ -5,11 +5,12 @@ include_once '../include/config.php';
 db('mpay');
 $today = date("Y-m-d");
 $time = strtotime($today . " 00:00:00");
-$sql = "select sum(price) as today_money from charge where time >= $time";
+$sql = "select sum(price) as today_money, sum(real_price) as today_real_money from charge where time >= $time";
 $res = mysql_query($sql);
 $today_money = 0;
 while($row = mysql_fetch_assoc($res)){
-   $today_money = $row['today_money'] ? $row['today_money'] : 0;
+    $today_money = $row['today_money'] ? $row['today_money'] : 0;
+    $today_real_money = $row['today_real_money'] ? $row['today_real_money'] : 0;
 }
 
 $sql = "select sum(price) as exception_money from charge_exception where clientTime like '$today%' and status=0";
@@ -23,6 +24,7 @@ $Smarty->assign(
     array(
     'exception_money'=>$exception_money,
     'today_money'=>$today_money,
+    'today_real_money'=>$today_real_money,
     )
 );
 ?>
